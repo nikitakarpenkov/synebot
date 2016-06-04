c8964e6b57f14b84c52b7c0be12ba8ce4ea8dc0d
Nikita Karpenkov <nikita.karpenkov@gmail.com>
2016-06-04 03:46:40 +0300

    initial commit

---
+++ bin/bot.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+'use strict';
+
+/**
+ * Bot launcher script.
+ *
+ * @authors Nikita Karpenkov <nikita.karpenkov@gmail.com>, Luciano Mammino <lucianomammino@gmail.com>
+ */
+
+var SyneBot = require('../lib/synebot');
+
+/**
+ * Environment variables used to configure the bot:
+ *
+ *  BOT_API_KEY : the authentication token to allow the bot to connect to your slack organization. You can get your
+ *      token at the following url: https://<yourorganization>.slack.com/services/new/bot (Mandatory)
+ *  BOT_NAME: the username you want to give to the bot within your organisation.
+ */
+var token = process.env.BOT_API_KEY || require('../token');
+var name = process.env.BOT_NAME;
+
+var handlers = [];
+
+var HelloHandler = function Constructor() {
+	this.answer = 'I\'m alive!';
+};
+
+HelloHandler.prototype.handle = function(message, context, replyCallback) {
+	replyCallback(this.answer + context.messages.length, null, true);
+};
+
+handlers.push(new HelloHandler());
+
+var synebot = new SyneBot({
+    token: token,
+    name: name
+}, handlers);
+
+synebot.run();
